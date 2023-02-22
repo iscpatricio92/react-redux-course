69db2dee6ccb5045cb2a11a1fc65c13bc16cba36
Patricio Gomez <isc.patricio@gmail.com>
2023-02-21 20:45:26 -0600

    project finished

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,5 +1,18 @@
 import { Input } from "antd";
+import { useDispatch } from "react-redux";
+import { setFilter } from "../slices/dataSlice";
+
 const Searcher = () => {
-  return <Input.Search placeholder="buscar" style={{ marginBottom: 10 }} />;
+  const dispatch = useDispatch();
+  const handleOnchange = (e) => {
+    dispatch(setFilter(e.target.value));
+  };
+  return (
+    <Input.Search
+      onChange={handleOnchange}
+      placeholder="buscar"
+      style={{ marginBottom: 10 }}
+    />
+  );
 };
 export default Searcher;
--- src/slices/dataSlice.js
+++ src/slices/dataSlice.js
@@ -4,6 +4,7 @@ import { setLoading } from "./uiSlice";
 
 const initialState = {
   pokemons: [],
+  pokemonsFiltered: [],
 };
 
 export const fetchPokemonsWithDetails = createAsyncThunk(
@@ -25,18 +26,38 @@ export const dataSlice = createSlice({
   reducers: {
     setPokemons: (state, action) => {
       state.pokemons = action.payload;
+      state.pokemonsFiltered = action.payload;
     },
     setFavorite: (state, action) => {
-      const currentPokemonIndex = state.pokemons.findIndex((pokemon) => {
-        return pokemon.id === action.payload.pokemonId;
-      });
+      const currentPokemonIndex = state.pokemons.findIndex(
+        (pokemon) => pokemon.id === action.payload.pokemonId
+      );
+      const currentFilteredPokemonIndex = state.pokemonsFiltered.findIndex(
+        (pokemon) => {
+          return pokemon.id === action.payload.pokemonId;
+        }
+      );
+
       if (currentPokemonIndex >= 0) {
         const isFavorite = state.pokemons[currentPokemonIndex].favorite;
         state.pokemons[currentPokemonIndex].favorite = !isFavorite;
       }
+
+      if (currentFilteredPokemonIndex) {
+        const isFavorite =
+          state.pokemonsFiltered[currentFilteredPokemonIndex].favorite;
+        state.pokemonsFiltered[currentFilteredPokemonIndex].favorite =
+          !isFavorite;
+      }
+    },
+    setFilter: (state, action) => {
+      const pokemonsFiltered = state.pokemons.filter((pokemon) =>
+        pokemon.name.includes(action.payload)
+      );
+      state.pokemonsFiltered = pokemonsFiltered;
     },
   },
 });
 
-export const { setFavorite, setPokemons } = dataSlice.actions;
+export const { setFavorite, setPokemons, setFilter } = dataSlice.actions;
 export default dataSlice.reducer;
